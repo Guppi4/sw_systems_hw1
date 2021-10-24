ad4404147f3a7dfe4bc7028715964668fc8f320e
Guppi4 <kirill.perevalov.1996@gmail.com>
2021-10-25 00:10:04 +0300

    5

--- basicClassification.c
+++ basicClassification.c
@@ -1,2 +1,16 @@
-#include <stdio.h>;
-#include"NumClass.h";
+#include <stdio.h>
+#include"NumClass.h"
+
+int isPrime(int p){
+ 
+ int i,prime=0;
+ 
+ for (i = 2; i <= p / 2; ++i) {
+   
+    if (p % i == 0) {
+      prime = 1;
+      break;
+    }
+  }
+    return prime;
+  }
